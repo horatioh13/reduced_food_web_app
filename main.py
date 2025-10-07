2664865a0464505bacc18753d8a5fdd6c2fc3bb1
horatioh13 <horatioh13@gmail.com>
2025-10-07 19:27:52 +0100

    fixed app creation process

--- main.py
+++ main.py
@@ -383,7 +383,9 @@ def create_app():
 
     return app
 
+# Make the module-level "app" the wrapped app so Gunicorn imports the correct WSGI app
+app = create_app()
+
 if __name__ == '__main__':
-    init_db()
     app.run(host='0.0.0.0', port=5000, debug=True)
     
